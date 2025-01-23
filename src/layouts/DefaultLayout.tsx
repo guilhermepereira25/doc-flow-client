c3277433b24319187d2ea1cf4c2c7b6e9385e13f
devgui <guihermepereiradv@gmail.com>
2025-01-23 11:24:20 -0300

    refactor: adjust layouts

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,9 +1,5 @@
 import NavBar from '@/components/NavBar';
 
 export default function DefaultLayout() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+  return <NavBar />;
 }
