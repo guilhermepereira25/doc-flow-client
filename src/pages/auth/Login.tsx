2546734ff955f056b63eb37fa492db0159bfcb3f
devgui <guihermepereiradv@gmail.com>
2025-01-24 12:42:57 -0300

    refactor(src/pages/auth): adjust login pages

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -4,17 +4,14 @@ import { getAccessToken } from '@/api/data/auth.data';
 import { useNavigate } from 'react-router';
 import useAuth from '@/hooks/useAuth';
 import AuthForm from '@/components/AuthForm';
-import { useEffect, useState } from 'react';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { AlertCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { authFormSchema, type AuthFormSchema } from '@/lib/types';
+import useAuthError from '@/hooks/useAuthError';
 
 export default function Login() {
   const navigate = useNavigate();
   const { setToken, setIsAuthenticated } = useAuth();
-  const [error, setError] = useState<string | null>(null);
-  const [text, setText] = useState<string>('');
+  const { setError } = useAuthError();
 
   const form = useForm<AuthFormSchema>({
     resolver: zodResolver(authFormSchema),
@@ -35,44 +32,8 @@ export default function Login() {
     navigate('/');
   };
 
-  useEffect(() => {
-    if (error) {
-      setTimeout(() => {
-        setError(null);
-      }, 5000);
-    }
-  }, [error]);
-
-  useEffect(() => {
-    const texts = [
-      'Acesse seu evento',
-      'Crie e adicione eventos',
-      'Acesse, crie e adicione eventos',
-    ];
-    let index = 0;
-    const interval = setInterval(() => {
-      setText(texts[index]);
-      index = index === texts.length - 1 ? 0 : index + 1;
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div className="w-full max-w-[400px] space-y-3">
-      <h2 className="text-center text-4xl text-sky-900 transition-opacity duration-5000 ease-in-out opacity-0 animate-fade-in-out">
-        {text}
-      </h2>
-      <p className="text-center text-base text-sky-900">
-        Acesse, crie e adicione eventos
-      </p>
-
-      {error && (
-        <Alert variant="destructive">
-          <AlertCircle className="h-4 w-4" />
-          <AlertTitle>Ah, não</AlertTitle>
-          <AlertDescription>{error}</AlertDescription>
-        </Alert>
-      )}
+    <>
       <AuthForm onSubmit={handleSubmit} form={form} />
 
       <p className="text-sky-900">Não possui conta?</p>
@@ -82,6 +43,6 @@ export default function Login() {
       >
         Cadastrar
       </Button>
-    </div>
+    </>
   );
 }
