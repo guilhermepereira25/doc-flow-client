5132b1a51d48cd55e714c03a87913722481f0099
devgui <guihermepereiradv@gmail.com>
2025-01-23 22:02:14 -0300

    refactor(src/pages/auth/Login): use AuthForm as form component, implements error handling, add zod to form validation

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,50 +1,103 @@
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
 import { useForm } from 'react-hook-form';
-import logo from '../../assets/cefet-logo.png';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { getAccessToken } from '@/api/data/auth.data';
+import { useNavigate } from 'react-router';
+import useAuth from '@/hooks/useAuth';
+import AuthForm from '@/components/AuthForm';
+import { useEffect, useState } from 'react';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { AlertCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
+const formSchema = z.object({
+  email: z
+    .string()
+    .email({
+      message: 'Insira um email válido.',
+    })
+    .regex(/@cefet-rj.br$/, {
+      message: 'Insira um email institucional.',
+    }),
+  password: z.string().min(2, {
+    message: 'A senha deve ter no mínimo 8 caracteres.',
+  }),
+});
+
+export type FormSchema = z.infer<typeof formSchema>;
+
 export default function Login() {
-  const form = useForm();
+  const navigate = useNavigate();
+  const { setToken, setIsAuthenticated } = useAuth();
+  const [error, setError] = useState<string | null>(null);
+  const [text, setText] = useState<string>('');
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const handleSubmit = async (data: FormSchema) => {
+    const accessToken = await getAccessToken(data.email, data.password);
+    if (!accessToken) {
+      setError('Não foi possível fazer login. Tente novamente.');
+      return;
+    }
+    localStorage.setItem('accessToken', accessToken);
+    setToken(accessToken);
+    setIsAuthenticated(true);
+    navigate('/');
+  };
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError(null);
+      }, 5000);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    const texts = [
+      'Acesse seu evento',
+      'Crie e adicione eventos',
+      'Acesse, crie e adicione eventos',
+    ];
+    let index = 0;
+    const interval = setInterval(() => {
+      setText(texts[index]);
+      index = index === texts.length - 1 ? 0 : index + 1;
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="flex items-center justify-center flex-col h-screen">
-      <div className="logo">
-        <img className="h-20" src={logo} alt="Logo" />
-      </div>
+    <div className="w-full max-w-[400px] space-y-3">
+      <h2 className="text-center text-4xl text-sky-900 transition-opacity duration-5000 ease-in-out opacity-0 animate-fade-in-out">
+        {text}
+      </h2>
+      <p className="text-center text-base text-sky-900">
+        Acesse, crie e adicione eventos
+      </p>
 
-      <div>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit((data) => console.log(data))}>
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="userhappy" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+      {error && (
+        <Alert variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Ah, não</AlertTitle>
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
+      )}
+      <AuthForm onSubmit={handleSubmit} form={form} />
 
-            <Button type="submit">Login</Button>
-          </form>
-        </Form>
-      </div>
+      <p className="text-sky-900">Não possui conta?</p>
+      <Button
+        className="w-full bg-sky-900 text-white hover:bg-sky-800"
+        onClick={() => navigate('/signup')}
+      >
+        Cadastrar
+      </Button>
     </div>
   );
 }
