253eba4c9da63a33e0ee842b6dc4761bea77e663
devgui <guihermepereiradv@gmail.com>
2025-01-23 22:03:12 -0300

    refactor(src/layouts/AuthLayout): implements logo div

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,9 +1,19 @@
 import { Outlet } from 'react-router';
+import logo from '@/assets/cefet-logo.png';
 
 export default function AuthLayout() {
   return (
     <div className="h-screen grid grid-cols-2">
-      <Outlet />
+      <div className="flex flex-col min-h-screen items-center justify-center bg-white p-4">
+        <div className="logo">
+          <img
+            className="mx-auto h-[200px] w-auto object-contain"
+            src={logo}
+            alt="CEFET/RJ"
+          />
+        </div>
+        <Outlet />
+      </div>
       <div className="bg-sky-900"></div>
     </div>
   );
