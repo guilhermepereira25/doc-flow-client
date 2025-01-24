bfb1b66e46e868e21ad2d3eb2997b34552105a5f
devgui <guihermepereiradv@gmail.com>
2025-01-24 10:56:25 -0300

    fix(src/componens/PrivateRoute): use outlet instead of render children

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import { Navigate } from 'react-router';
+import { Navigate, Outlet } from 'react-router';
 import useAuth from '@/hooks/useAuth';
 
-interface PrivateRouteProps {
-  children: JSX.Element;
-}
-
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
+const PrivateRoute = () => {
   const { isAuthenticated } = useAuth();
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
