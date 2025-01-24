1159254b35abc17c52550baf3d3e9ea66dfb248c
devgui <guihermepereiradv@gmail.com>
2025-01-23 22:00:14 -0300

    feat(src/lib/types.ts): adjust AuthContextType interface

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,13 @@
 export interface User {
-    username: string;
+    email: string;
     password: string;
     profiles: string[];
 }
 
 export interface AuthContextType {
     token: string | null;
+    setToken: (token: string | null) => void;
     logout: () => void;
     isAuthenticated: boolean;
+    setIsAuthenticated: (isAuthenticated: boolean) => void;
 }
