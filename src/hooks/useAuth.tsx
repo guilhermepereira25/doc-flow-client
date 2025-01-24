b0127b55debd8e5cbfb75dbbd1f7544f2d6c1299
devgui <guihermepereiradv@gmail.com>
2025-01-23 21:57:19 -0300

    refactor(useAuth): export as default

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,8 +1,10 @@
 import { useContext } from 'react';
-import { AuthContext } from '@/context/AuthContext';
+import { AuthContext } from '@/context/AuthProvider';
 
-export const useAuth = () => {
+const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) throw new Error('useAuth must be used within an AuthProvider');
   return context;
 };
+
+export default useAuth;
