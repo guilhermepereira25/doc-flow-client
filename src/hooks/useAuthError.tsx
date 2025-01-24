55a12eafe70953eb5007de1e400261f5a670e366
devgui <guihermepereiradv@gmail.com>
2025-01-24 12:44:32 -0300

    feat(src/hooks): implements useAuthError hook

---
+++ src/hooks/useAuthError.tsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { AuthErrorContext } from '@/layouts/AuthLayout';
+
+const useAuthError = () => {
+  const context = useContext(AuthErrorContext);
+  if (context === undefined) {
+    throw new Error(
+      'useAuthErrorContext must be used within a AuthErrorContext.Provider'
+    );
+  }
+  return context;
+};
+
+export default useAuthError;
