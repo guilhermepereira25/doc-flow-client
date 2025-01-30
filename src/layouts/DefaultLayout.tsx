fcd865c9a5c47e872d37df5e21b2b3257718496c
devgui <guihermepereiradv@gmail.com>
2025-01-29 22:53:10 -0300

    feat(DefaultLayout): change default layout add sidebar

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,5 +1,18 @@
-import NavBar from '@/components/NavBar';
+import { AppSidebar } from '@/components/AppSidebar';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { Outlet } from 'react-router';
 
 export default function DefaultLayout() {
-  return <NavBar />;
+  return (
+    <div className="h-screen">
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <main>
+            <Outlet />
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
 }
