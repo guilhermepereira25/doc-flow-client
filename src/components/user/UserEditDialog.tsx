b42d6e32b68c5bf6b2bdd3db678aab1e70f3d349
devgui <guihermepereiradv@gmail.com>
2025-02-24 16:28:29 -0300

    feat: refactor UserEditDialog to use new form components and improve user data handling

--- src/components/user/UserEditDialog.tsx
+++ src/components/user/UserEditDialog.tsx
@@ -9,45 +9,50 @@ import {
 } from '@/components/ui/dialog';
 import { Button } from '../ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { useForm } from 'react-hook-form';
-import { CreateUser, createUserSchema, User } from '@/lib/schemas/user.schema';
-import useAuth from '@/hooks/useAuth';
+import { CreateUser, createUserSchema } from '@/lib/schemas/user.schema';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useState } from 'react';
-import { getUser } from '@/api/data/users.data';
 import { toast } from 'sonner';
-import { Select } from '../ui/select';
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+} from '../ui/select';
+import { cn } from '@/lib/utils';
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form';
+import useProfile from '@/hooks/useProfile';
+import { updateUserPatchVerb } from '@/api/data/users.data';
+import useUser from '@/hooks/useUser';
+import useProfiles from '@/hooks/useProfiles';
 
 export default function UserEditDialog() {
-  const [loadedUser, setLoadedUser] = useState<User | null>(null);
-  const { user } = useAuth();
+  const profiles = useProfiles();
+  const { user } = useUser();
+  const { isUserProfileAdminOrProfessor } = useProfile();
 
   const form = useForm<CreateUser>({
     resolver: zodResolver(createUserSchema),
-    defaultValues: {
-      fullName: user?.fullName || '',
-      email: user?.email || '',
-      enrollment: loadedUser?.enrollment || '',
-      profile_id: user?.profile.name || '',
+    defaultValues: async () => {
+      return {
+        fullName: user?.full_name || '',
+        email: user?.email || '',
+        enrollment: user?.enrollment || '',
+        profile_id: user?.profile_id || '',
+      };
     },
   });
 
-  const fetchUser = async () => {
+  const handleSubmit = async (data: CreateUser) => {
     if (!user) return;
-    const currentUser = await getUser(user.sub);
-    if (currentUser) {
-      if (import.meta.env.DEV) toast.info('Usuário carregado com sucesso');
-      setLoadedUser(currentUser);
+    const updatedUser = await updateUserPatchVerb(user.id, data);
+    if (updatedUser) {
+      toast.success('Usuário atualizado com sucesso');
       return;
     }
-    toast.error('Erro ao carregar usuário');
-  }
-
-  useEffect(() => {
-    fetchUser();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    toast.error('Erro ao atualizar usuário');
+  };
 
   return (
     <Dialog onOpenChange={(isOpen) => !isOpen && form.reset()}>
@@ -67,62 +72,132 @@ export default function UserEditDialog() {
             alterações"
           </DialogDescription>
         </DialogHeader>
-          <form onSubmit={form.handleSubmit((data) => data)}>
+        <Form {...form}>
+          <form id="user-edit" onSubmit={form.handleSubmit(handleSubmit)}>
             <div className="grid gap-4 py-4">
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="fullName" className="text-right">
-                  Nome completo
-                </Label>
-                <Input
-                  id="fullName"
-                  {...form.register('fullName')}
-                  placeholder="Nome completo"
-                  defaultValue={user?.fullName}
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="email" className="text-right">
-                  Email
-                </Label>
-                <Input
-                  id="email"
-                  {...form.register('email')}
-                  placeholder="Email"
-                  defaultValue={user?.email}
-                  className="col-span-3"
-                />
-              </div>
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="enrollment" className="text-right">
-                  Matricula
-                </Label>
-                <Input
-                  id="enrollment"
-                  {...form.register('enrollment')}
-                  placeholder="1111222SINF"
-                  defaultValue={loadedUser?.enrollment || ''}
-                  className="col-span-3"
-                />
-              </div>
-              <div className='grid grid-cols-4 items-center gap-4'>
-                <Label htmlFor='profile' className='text-right'>
-                  Perfil
-                </Label>
-                <Select>
-                  
-                </Select>
-              </div>
+              <FormField
+                control={form.control}
+                name="fullName"
+                defaultValue={user?.full_name || ''}
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel className="text-right">Nome completo</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={cn(
+                          'rounded-2xl bg-white bg-opacity-60',
+                          form.formState.errors?.fullName &&
+                            'border-destructive',
+                          'col-span-3'
+                        )}
+                        type="text"
+                        placeholder="Nome completo"
+                        {...field}
+                        required
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                defaultValue={user?.email || ''}
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel className="text-right">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={cn(
+                          'rounded-2xl bg-white bg-opacity-60',
+                          form.formState.errors?.email && 'border-destructive',
+                          'col-span-3'
+                        )}
+                        type="email"
+                        placeholder="Email"
+                        {...field}
+                        onInput={() => {
+                          if (form.formState.errors.email?.message) {
+                            toast.error(form.formState.errors.email.message);
+                          }
+                        }}
+                        required
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="enrollment"
+                defaultValue={user?.enrollment || ''}
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel className="text-right">Matricula</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={cn(
+                          'rounded-2xl bg-white bg-opacity-60',
+                          form.formState.errors.enrollment?.message &&
+                            'border-destructive',
+                          'col-span-3'
+                        )}
+                        type="text"
+                        placeholder="Matricula"
+                        {...field}
+                        onInput={() => {
+                          if (form.formState.errors.enrollment?.message) {
+                            toast.error(
+                              form.formState.errors.enrollment.message
+                            );
+                          }
+                        }}
+                        required
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="profile_id"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel className="text-right">Perfil</FormLabel>
+                    <FormControl>
+                      <Select
+                        defaultValue={field.value}
+                        onValueChange={field.onChange}
+                        disabled={!isUserProfileAdminOrProfessor}
+                      >
+                        <SelectTrigger className="col-span-3 rounded-2xl">
+                          <SelectValue placeholder="Selecione um perfil" />
+                        </SelectTrigger>
+
+                        <SelectContent>
+                          {profiles.map((profile) => (
+                            <SelectItem key={profile.id} value={profile.id}>
+                              {profile.name}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
             </div>
             <DialogFooter>
               <Button
                 type="submit"
                 className="bg-sky-900 rounded-2xl text-white"
+                form="user-edit"
               >
                 Salvar alterações
               </Button>
             </DialogFooter>
           </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
