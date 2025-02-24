3e094f71ad02f06e844b3af5bcfa3e022aca6919
devgui <guihermepereiradv@gmail.com>
2025-02-23 23:57:42 -0300

    feat: implement user retrieval functions in API service

--- src/api/data/users.data.ts
+++ src/api/data/users.data.ts
@@ -7,8 +7,19 @@ type User = components['schemas']['User'];
 export const getAllUsers = async (): Promise<User[] | undefined> => {
     try {
         const userService = new UsersService();
-        const response: UsersGetAllResponse = await userService.getAll();
+        const response: UsersGetAllResponse = await userService.getAll({ limit: 10, offset: 0 });
         return response?.data.users;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getUser = async (id: string): Promise<User | undefined> => {
+    try {
+        const userService = new UsersService();
+        const response = await userService.getOne(id);
+        console.log(response.data.user);
+        return response.data.user;
     } catch (error) {
         console.error(error);
     }
--- src/api/services/users.service.ts
+++ src/api/services/users.service.ts
@@ -1,7 +1,9 @@
+import { ApiResponse } from '@/lib/types';
 import AbstractService from './abstract.service';
 import type { components } from '@/lib/schema';
 
 type UsersGetAllResponse = components['schemas']['GetAllUsersResponseDto'];
+type User = components['schemas']['User'];
 
 export default class UserService extends AbstractService {
   constructor() {
@@ -10,5 +12,9 @@ export default class UserService extends AbstractService {
 
   async getAll(args: { limit: number, offset: number }): Promise<UsersGetAllResponse> {
     return await this.api.get(this.basePath + `?limit=${args.limit}&offset=${args.offset}`);
+  }
+
+  async getOne(id: string): Promise<ApiResponse<{ user: User }>> {
+    return await this.api.get(this.basePath + `/${id}`);
   }
 }
