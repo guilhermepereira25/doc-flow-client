1268981f88ca89abd42c95af1d1d967d93f5121e
devgui <guihermepereiradv@gmail.com>
2025-02-24 16:28:21 -0300

    feat: enhance user schema validation with custom error messages for email and enrollment

--- src/lib/schemas/user.schema.ts
+++ src/lib/schemas/user.schema.ts
@@ -8,8 +8,12 @@ export const createUserSchema = z.object({
   email: z
     .string()
     .email()
-    .regex(/@cefet-rj.br$/),
-  enrollment: z.string().regex(/\d{4}\d{3}\w{3}/),
+    .regex(/@cefet-rj.br$/, {
+      message: "Email inválido",
+    }),
+  enrollment: z.string().regex(/\d{4}\d{3}\w{3}/, {
+    message: "Matrícula inválida",
+  }),
   profile_id: z.string(),
 });
 
