f73372fd72fd36f0c810bc28300ab8586191fb79
devgui <guihermepereiradv@gmail.com>
2025-02-26 14:57:47 -0300

    feat: update ViewFileTableColumns to include status and event columns with localized date formatting

--- src/components/files/ViewFileTableColumns.tsx
+++ src/components/files/ViewFileTableColumns.tsx
@@ -1,35 +1,53 @@
 import { ColumnDef } from "@tanstack/react-table";
-import { File } from "@/lib/schemas/file";
+import { File } from '@/lib/schemas/file.schema';
 
 export function getColumns(): ColumnDef<File>[] {
   return [
     {
-      accessorKey: "name",
-      header: "Nome",
+      accessorKey: 'name',
+      header: 'Nome',
       enableHiding: false,
     },
     {
-      accessorKey: "type",
-      header: "Tipo de arquivo",
+      accessorKey: 'type',
+      header: 'Tipo de arquivo',
       enableHiding: false,
     },
     {
-      accessorKey: "user",
-      header: "Responsável",
+      accessorKey: 'status',
+      header: 'Status',
+      cell: ({ row }) => {
+        switch (row.original.status) {
+          case 'waiting':
+            return 'Aguardando';
+          case 'done':
+            return 'Concluído';
+          default:
+            return 'Em andamento';
+        }
+      },
+    },
+    {
+      accessorKey: 'user',
+      header: 'Responsável',
       cell: ({ row }) => {
         return row.original?.user?.full_name;
       },
       enableHiding: false,
     },
     {
-      accessorKey: "created_at",
-      header: "Data de criação",
+      accessorKey: 'event_id',
+      header: 'Evento',
+    },
+    {
+      accessorKey: 'created_at',
+      header: 'Data de criação',
       cell: ({ row }) => {
         const createdAtDate = new Date(row.original.created_at);
         return (
-          createdAtDate.toLocaleDateString("pt-br") +
-          " " +
-          createdAtDate.toLocaleTimeString("pt-br")
+          createdAtDate.toLocaleDateString('pt-br') +
+          ' ' +
+          createdAtDate.toLocaleTimeString('pt-br')
         );
       },
       enableHiding: true,
