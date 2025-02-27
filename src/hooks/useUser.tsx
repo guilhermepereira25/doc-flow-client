6e7ea652ad973865169f9c442ec6b5467ce73d20
devgui <guihermepereiradv@gmail.com>
2025-02-27 15:52:35 -0300

    feat: add loading state to useUser hook for improved user feedback during data fetching

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -5,35 +5,42 @@ import { toast } from 'sonner';
 import { User } from '@/lib/schemas/user.schema';
 
 interface UseUser {
-    user: User | null;
-    setUser: (user: User | null) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
+  isLoading: boolean;
 }
 
 const useUser = (): UseUser => {
-    const [loadedUser, setLoadedUser] = useState<User | null>(null);
-    const { user } = useAuth();
-    if (!user) {
-        throw new Error('useUser must be used within an AuthProvider');
+  const [loadedUser, setLoadedUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { user } = useAuth();
+  if (!user) {
+    throw new Error('useUser must be used within an AuthProvider');
+  }
+
+  const fetchUser = useCallback(async () => {
+    toast.info('Carregando usuário...');
+    setIsLoading(true);
+    const currentUser = await getUser(user.sub);
+    if (currentUser) {
+      if (import.meta.env.DEV) toast.info('Usuário carregado com sucesso');
+      setLoadedUser(currentUser);
+      setIsLoading(false);
+      return;
     }
+    toast.error('Erro ao carregar usuário');
+    setIsLoading(false);
+  }, [user.sub]);
 
-    const fetchUser = useCallback(async () => {
-        const currentUser = await getUser(user.sub);
-        if (currentUser) {
-            if (import.meta.env.DEV) toast.info('Usuário carregado com sucesso');
-            setLoadedUser(currentUser);
-            return;
-        }
-        toast.error('Erro ao carregar usuário');
-    }, [user.sub]);
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
 
-    useEffect(() => {
-        fetchUser();
-    }, [fetchUser]);
-
-    return {
-        user: loadedUser,
-        setUser: setLoadedUser,
-    }
+  return {
+    user: loadedUser,
+    setUser: setLoadedUser,
+    isLoading,
+  };
 };
 
 export default useUser;
